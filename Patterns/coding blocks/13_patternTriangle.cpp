509d9ece3833b7d3ba43976df9bf7d7a9e22d46f
manasrathore3 <manasrathore93@gmail.com>
2026-03-13 11:11:37 +0530

    13_pattern

--- Patterns/coding blocks/13_patternTriangle.cpp
+++ Patterns/coding blocks/13_patternTriangle.cpp
@@ -7,51 +7,36 @@ int main(){
     cout<<"Enter N :" ;
     cin>>n;
 
-    // for (int i = 0; i < n; i++){
-    //     for (int j = 0; j < i+1; j++){
-    //         cout<<"* ";
-    //     }cout<<endl;
-    // }
-    // for(int i = 0;  i < n-1; i++){
-    //     for(int j = 0; j < n-i-1; j++){
-    //         cout<<"* ";
-    //     }cout<<endl;
-    // }   
+    for (int i = 0; i < n; i++){               //13th ques normal type
+        for (int j = 0; j < i+1; j++){
+            cout<<"* ";
+        }cout<<endl;
+    }
+    for(int i = 0;  i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            cout<<"* ";
+        }cout<<endl;
+    }   
       
     
-    // for (int i = 0; i < n; i++){
-    //     for (int j = 0; j < n-i-1; j++){
-    //         cout<<"  ";
-    //     }
-    //     for (int j = 0; j < i+1; j++){
-    //         cout<<"* ";
-    //     }cout<<endl;
-    // }
-    // for(int i = 0;  i < n-1; i++){
-    //     for(int j = 0; j < i+1; j++){
-    //         cout<<"  ";
-    //     }
-    //     for(int j = 0; j < n-i-1; j++){
-    //         cout<<"* ";
-    //     }cout<<endl;
-    // }
-    
-
-    for (int i = 0; i < 2*n-1; i++){
+    for (int i = 0; i < n; i++){               // 14th ques opposite type
         for (int j = 0; j < n-i-1; j++){
             cout<<"  ";
         }
         for (int j = 0; j < i+1; j++){
             cout<<"* ";
-        }cout<<endl; 
-        // if(i>=n){
-        //        for(int j = 0; j < i+1; j++){
-        //         cout<<"  ";
-        //     }
-        //     for(int j = 0; j < n-i-1; j++){
-        //         cout<<"* ";
-        //     }
-        // }
+        }cout<<endl;
     }
+    for(int i = 0;  i < n-1; i++){
+        for(int j = 0; j < i+1; j++){
+            cout<<"  ";
+        }
+        for(int j = 0; j < n-i-1; j++){
+            cout<<"* ";
+        }cout<<endl;
+    }
+    
+
+    
     return 0;
 }
---
+++ Patterns/coding blocks/15_design.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+
+    int n,num;
+    cout<<"Enter N :" ;
+    cin>>n;
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < i*2; j++){
+            cout<<"  ";
+        }
+        for (int j = 0; j < n-i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < 2*n-4-2*i; j++){
+            cout<<"  ";
+        }
+        for (int j = 0; j < i+2; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    
+    return 0;
+
+}    
