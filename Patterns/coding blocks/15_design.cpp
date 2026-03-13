94b92cfcf2fdee1f13962e93fe2994d1c17f52fe
manasrathore3 <manasrathore93@gmail.com>
2026-03-13 17:07:37 +0530

    question 15

--- Patterns/coding blocks/15_design.cpp
+++ Patterns/coding blocks/15_design.cpp
@@ -7,24 +7,45 @@ int main(){
     cout<<"Enter N :" ;
     cin>>n;
 
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < i*2; j++){
+    //         cout<<"  ";
+    //     }
+    //     for (int j = 0; j < n-i; j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for (int i = 0; i < n-1; i++){
+    //     for (int j = 0; j < 2*n-4-2*i; j++){
+    //         cout<<"  ";
+    //     }
+    //     for (int j = 0; j < i+2; j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < i*2; j++){
+        for (int j = 0; j < n-i-1; j++){
             cout<<"  ";
         }
         for (int j = 0; j < n-i; j++){
             cout<<"* ";
-        }
-        cout<<endl;
+        }cout<<endl;   
     }
     for (int i = 0; i < n-1; i++){
-        for (int j = 0; j < 2*n-4-2*i; j++){
+        for (int j = 0; j < i+1; j++){
             cout<<"  ";
         }
         for (int j = 0; j < i+2; j++){
             cout<<"* ";
-        }
-        cout<<endl;
+        }cout<<endl;
+        
+        
     }
+    
+    
     
     
     return 0;
