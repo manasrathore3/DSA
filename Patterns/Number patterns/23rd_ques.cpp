d164eb39650b904db9f5f015fc48640d9bb83e09
manasrathore3 <manasrathore93@gmail.com>
2026-03-15 14:21:08 +0530

    Pyramid changing number

--- Patterns/Number patterns/23rd_ques.cpp
+++ Patterns/Number patterns/23rd_ques.cpp
@@ -7,16 +7,28 @@ int main(){
     cout<<"Enter N : ";
     cin>>n;
 
-    for (int i = 0; i < n; i++){                      //23
+    // for (int i = 0; i < n; i++){                      //23
+    //     for (int j = 0; j < n-i-1; j++){
+    //         cout<<"  ";
+    //     }
+    //     for (int j = 0; j < 2*i+1; j++){
+    //         cout<<"1 ";
+    //     }
+    //     cout<<endl;
+    // }
+      
+    num=1;
+    for (int i = 0; i < n; i++){                        //24
         for (int j = 0; j < n-i-1; j++){
             cout<<"  ";
         }
         for (int j = 0; j < 2*i+1; j++){
-            cout<<"1 ";
+            cout<<num<<" ";
         }
         cout<<endl;
+        num=num+1;
     }
-      
+
 
      
     return 0;
