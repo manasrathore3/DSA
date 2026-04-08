465447bab7b0d201badae34b46f56f894b9b90bb
manasrathore3 <manasrathore93@gmail.com>
2026-04-08 21:59:01 +0530

    sheet_questions

--- Functions/Questions.cpp
+++ Functions/Questions.cpp
@@ -1,11 +1,37 @@
 #include<iostream>
 using namespace std;
-int factorial(int n){
-     
-    int fact=1;
-    for (int i = 1; i <= n; i++){
-        fact*=i;
-    }
+// void primeCheck(int n){
     
-    return fact;
+//     int count=0;
+//     for (int i = 2; i < n; i++){
+//         if (n%i==0){
+//             count++;
+//         }
+//     }
+//     if(count>0){
+//         cout<<"Number not prime"<<endl;
+//     }
+//     else{
+//         cout<<"Number is prime"<<endl; 
+//     }
+    
+// }
+
+int primeCheck(int n){
+    
+    for (int i = 3; i < n-1; i++){
+        if (n%i==0){
+            cout<<"Not Prime"<<endl;
+        } 
+        else{
+            cout<<"Prime"<<endl;
+            return;
+        }
+    }
+
+}
+
+int main(){
+    primeCheck(41);
+    return 0;
 }
