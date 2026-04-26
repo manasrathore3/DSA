835683a6defdefe80cc151ab6b0ea61a90d237d1
manasrathore3 <manasrathore93@gmail.com>
2026-04-26 23:44:59 +0530

    Operators

---
+++ Functions/Bitwise Operators/AllOperators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 6; 
+    int b = 2;
+
+    // int z = a & b;         //AND 
+    // cout<<z<<endl;
+
+    // int z = a | b;         //OR
+    // cout<<z<<endl;
+    
+    // int z = a ^ b;         //XOR 
+    // cout<<z<<endl;
+
+    
+    return 0;
+}
