725f414d6f785bbc65afe08779ff690e0f93813e
manasrathore3 <manasrathore93@gmail.com>
2026-04-27 23:03:19 +0530

    vvvv

---
+++ Functions/Bitwise Operators/DataTypeModifiers.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+     
+    // cout<<sizeof(int)<<endl;
+
+    // signed int x = -10;
+    // cout<<x<<endl;
+    // cout<<sizeof(short int)<<endl;
+    
+    
+    signed int n; 
+    cout<<"Enter n:";
+    cin>>n;
+    int k,sum=0;
+    while (n>0){
+        k=n%10;
+        n=n/10;
+
+        sum+=k;
+        sum*=10;
+    }
+
+    cout<<sum<<endl;
+    
+    return 0;
+}
