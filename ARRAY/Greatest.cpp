7dff10775bac0c549a71d037dc12fb8ecd081b54
manasrathore3 <manasrathore93@gmail.com>
2026-04-29 23:06:08 +0530

    Greater Value

--- ARRAY/Greatest.cpp
+++ ARRAY/Greatest.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main(){
     
-    int marks[5]={4,2,3,0,-5};
-    int ans = INT_MAX;           //for infinite value 
+    int marks[5]={4,2,3,0,5};
+    int ans = INT_MIN;           //for -ve infinite value 
     int size = 5;
 
 
     for (int i = 0; i < size ; i++){
-        if (marks[i]<ans){
+        if (marks[i]>ans){
             ans = marks[i];
         } 
     }
