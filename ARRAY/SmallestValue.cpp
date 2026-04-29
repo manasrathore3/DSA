a946ac2e923f061cc719026dacf77220fd6b573b
manasrathore3 <manasrathore93@gmail.com>
2026-04-29 23:52:26 +0530

    Infinite Variable

--- ARRAY/SmallestValue.cpp
+++ ARRAY/SmallestValue.cpp
@@ -5,16 +5,16 @@ int main(){
     
     int marks[5]={4,2,3,0,-5};
     int size = 5;
-    int ans = INT_MAX;
+    int smallest = INT_MAX;
 
 
     for (int i = 0; i < size ; i++){
-        if (marks[i]<ans){
-            ans = marks[i];
+        if (marks[i]<smallest){
+            smallest = marks[i];
         } 
     }
     
-    cout<<"Smallest is :"<<ans<<endl;
+    cout<<"Smallest is :"<<smallest<<endl;
     
     return 0;
 }
