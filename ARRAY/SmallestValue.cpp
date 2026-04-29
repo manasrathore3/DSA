f4a0509c8f988c7fc73d63add8b53a077c42d9dc
manasrathore3 <manasrathore93@gmail.com>
2026-04-29 21:12:46 +0530

    Smallest element

--- ARRAY/SmallestValue.cpp
+++ ARRAY/SmallestValue.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
+#include <limits>                 //for infinite
 using namespace std;
 int main(){
     
     int marks[5]={4,2,3,0,-5};
-    int ans = 8;
+    int ans = std::numeric_limits<int>::max();       //for infinite value 
     int size = 5;
 
 
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size ; i++){
         if (marks[i]<ans){
             ans = marks[i];
         } 
