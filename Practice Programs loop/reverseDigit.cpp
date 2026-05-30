1ae393de93cf01e9fa7841b961fe1832089e0bd4
manasrathore3 <manasrathore93@gmail.com>
2026-05-30 23:19:35 +0530

    Unsigned Reverse

--- Practice Programs loop/reverseDigit.cpp
+++ Practice Programs loop/reverseDigit.cpp
@@ -3,17 +3,19 @@ using namespace std;
 int main(){
    
 
-    int n,rev,fin;
+    int n,rev;
+    int fin=0;
     cout<<"Enter N :" ;
     cin>>n;
     
-    while ( n > 0){
+    while ( n > 0){ 
         rev=n%10;
+        fin*=10; 
+        fin+=rev;   
         n/=10;
-        fin+=rev*10;
-    }    
 
-    
+    }    
+    cout<<fin;
 
     return 0;
 }    
