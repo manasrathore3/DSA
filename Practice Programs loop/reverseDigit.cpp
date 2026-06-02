884abf7e6ff704956508d2ec84073a26fca69c28
manasrathore3 <manasrathore93@gmail.com>
2026-06-02 22:34:51 +0530

    mdfic1

--- Practice Programs loop/reverseDigit.cpp
+++ Practice Programs loop/reverseDigit.cpp
@@ -3,19 +3,33 @@ using namespace std;
 int main(){
    
 
-    signed int n,rev;
-    signed int fin=0;
+    int n,rev;
+    int fin=0;
     cout<<"Enter N :" ;
     cin>>n;
     
-    while ( n > 0){ 
-        rev=n%10;
-        fin*=10; 
-        fin+=rev;   
-        n/=10;
-        
-    }    
-    cout<<fin;
+    if (n>0){
+        while ( n > 0){ 
+          rev=n%10;
+          fin*=10; 
+          fin+=rev;   
+          n/=10;
+        }
+        cout<<fin;
+    }else if (n<0){ 
+        n=n*(-1);
+        while ( n > 0){ 
+           rev=n%10;
+           fin*=10; 
+           fin+=rev;   
+           n/=10;
+        }
+
+        fin=fin*(-1);
+        cout<<fin;
+    }  
 
     return 0;
 }    
+
+
