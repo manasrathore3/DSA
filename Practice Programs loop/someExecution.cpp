52ac088dddaeb0a446a8e894efe647a65e39f781
manasrathore3 <manasrathore93@gmail.com>
2026-06-11 22:52:59 +0530

    After break

--- Practice Programs loop/someExecution.cpp
+++ Practice Programs loop/someExecution.cpp
@@ -2,37 +2,20 @@
 using namespace std;
 int main(){
      
-    int n,rev;
-    int fin=0;
+    int n,res=0;
+    int y=0;
+    int x=1;
     cout<<"Enter N:";
     cin>>n;
+  
+    for(int i=0;i<n;i++){
+            cout<<res<<endl;
+            res=x+y;
+            x=y;
+            y=res;
+        }
+
+    return 0;
+} 
         
-if (n>0){
-    while ( n > 0){ 
-        rev=n%10;
-        fin*=10; 
-        fin+=rev;   
-        n/=10;
-    }
-    cout<<fin<<endl;
-}   
-// cout<<n;
-
-if(fin>0){
-    while ( fin > 0){ 
-        rev=fin%10;
-        n*=10; 
-        n+=rev;   
-        fin/=10;
-    }
-    cout<<n<<endl;
-}  
-
-if(n==fin){
-   cout<<"Palindrome";
-}else{
-   cout<<"Not Palindrome";
-}
-
-}
     
