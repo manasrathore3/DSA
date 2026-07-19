05e01a6024064664583c45e0c92bafcc652477f1
manasrathore3 <manasrathore93@gmail.com>
2026-07-20 00:19:08 +0530

    Basic revise

--- Functions/firstFunc.cpp
+++ Functions/firstFunc.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 //function Definition
-int printHello(){        //int ya void Kuch bhi
+/*int printHello(){        //int ya void Kuch bhi
 
     cout<<"hello";
     return 3;
@@ -13,6 +13,34 @@ int main (){
     cout<< printHello();      // By this func get's called and print return value also
     return 0;
 }
-
+*/
 
 /////////////////////////////////////// PARAMETERS Le le 
+
+// int min(int a , int b){
+//     if (a<b){
+//         return a;
+//     }else{
+//         return b;
+//     }
+// }
+
+// int digitSum(int n){
+//     int sum=0;
+//     while (n>0){
+//         sum+=n%10;
+//         n=n/10;
+//     }
+
+//     cout<<sum;   
+// }
+
+
+
+int main(){
+    // cout<<min(1,5);
+    // digitSum(145);
+    return 0;
+}
+
+
